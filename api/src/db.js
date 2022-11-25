abfec5f626f20d6469dd410c2e48ca5504c8b3f4
Leandro Florentin <102199433+LeandroFlorentin@users.noreply.github.com>
2022-11-25 19:58:33 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -8,31 +8,7 @@ const {
 
 //Sequelize('postgres://user:pass@example.com:5432/dbname')
 
-const sequelize = 
-      process.env.NODE_ENV === 'production'
-        ? new Sequelize({
-            database: DB_NAME,
-            dialect: 'postgres',
-            username: DB_USER,
-            password: DB_PASSWORD,
-            host: DB_HOST,
-            port: DB_PORT,
-            dialect: 'postgres',
-            pool: {
-              max: 3,
-              min: 1,
-              idle: 10000,
-            },
-            dialectOptions: {
-              ssl: {
-                require: true,
-                rejectUnauthorized: false,
-              },
-              keepAlive: true,
-            },
-            ssl: true,
-          }) :
- new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/plantango`, {
+const sequelize =  new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
