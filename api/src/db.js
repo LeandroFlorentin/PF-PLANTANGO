2b163dfb50fab546ae701b30af5137573db2b818
Leandro Florentin <102199433+LeandroFlorentin@users.noreply.github.com>
2022-11-25 20:16:29 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, NODE_ENV
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, NODE_ENV, DB_DEPLOY
 } = process.env;
 
 //Sequelize('postgres://user:pass@example.com:5432/dbname')
